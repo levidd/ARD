0de669583438ba91533db108e8e447806a6a528e
weann2020 <anw2@unlv.nevada.edu>
2020-09-01 18:19:36 -0700

    players can pickup and drop items

--- src/com/mwl/characters/Player.java
+++ src/com/mwl/characters/Player.java
@@ -1,5 +1,6 @@
 package com.mwl.characters;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -25,15 +26,34 @@ public abstract class Player {
     }
 
 
-    //add the item picked up by the user into the item inventory collection
+    /*If the item is in current room, add the item picked up by the user into the item inventory
+    * and remove the item from the room item list
+    * if input is not in Item Enum, throw Exception
+    * */
     public void pickUpItem(Item item){
+        if(!Arrays.stream(Item.values()).anyMatch(item::equals)){
+            throw new IllegalArgumentException("Can't pick up! NO such item in this game!");
+        }
+        else if(currentRoom.getItems().contains(item)){
             itemsInventory.add(item);
+            currentRoom.grabItem(item);
+        } else {
+            System.out.println("Can't pick up! This item is not in the current room!");
+        }
+
     }
 
-    //remove the item dropped by the user from the item inventory collection
+    /*If the item is in player inventory, remove the item dropped by the user from the item inventory
+    * and add the item into the room item list
+    * if input is not in Item Enum, throw Exception
+    * */
     public void dropItem(Item item){
-        if(itemsInventory.contains(item)){
+        if(!Arrays.stream(Item.values()).anyMatch(item::equals)){
+            throw new IllegalArgumentException("Can't pick up! NO such item in this game!");
+        }
+        else if(itemsInventory.contains(item)){
             itemsInventory.remove(item);
+            currentRoom.addItem(item);
         } else {
             System.out.println("Can't drop this item! It's not in player's item inventory!");
         }

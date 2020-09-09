9fd83fa7c0d782a73b3a3b36e7ae329abe9cb97e
weann2020 <anw2@unlv.nevada.edu>
2020-09-09 13:11:10 -0700

    ready to pull from Iteration3

--- test/com/mwl/characters/PlayerTest.java
+++ test/com/mwl/characters/PlayerTest.java
@@ -2,33 +2,37 @@ package com.mwl.characters;
 
 import com.mwl.environment.Item;
 import com.mwl.environment.Room;
-import com.mwl.environment.RoomMap;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 
 public class PlayerTest {
-    Room room1 = new RoomMap().getStart();
+
     List<Item> playerInventory = new ArrayList<>();
     List<Item> roomInventory = new ArrayList<>();
+
+    Room room1 = new Room("room", 1);
     Player player = new Wolverine("Wolverine", 100, room1, playerInventory, 1);
+
 
 
     @Before
     public void setUp() throws Exception {
 
-
-
     }
 
     @Test
     public void testPickUpItem(){
-        roomInventory.add(Item.valueOf("food"));
-        player.pickUpItem(Item.valueOf("food"));
+        List<Item> list = new ArrayList<>();
+        room1.addItem(Item.valueOf("Food"));
+        player.pickUpItem(Item.valueOf("Food"));
 
+        assertEquals(player.getItemsInventory(), list.add(Item.valueOf("Food")));
     }
 
 }
